b1ba35adbb79fac29f0c8e3b92a70e9f85ffdb2b
Ava <createchivity@gmail.com>
2026-01-13 13:25:10 +0000

    fix install script

--- Waybar_Ultimate/install.sh
+++ Waybar_Ultimate/install.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-mkdir "Waybar-Backup"
-cp -r ~/.config/waybar ~/.config/waybar-backup
+cp -r ~/.config/waybar waybar-backup
 sudo rm -r ~/.config/waybar
 cp -r "$(pwd)" ~/.config/waybar
 chmod +x ~/.config/waybar/scripts/*.sh
-chmod +x install.sh
 echo "Waybar configuration installed to ~/.config/waybar"
 sudo systemctl --user restart waybar
 echo "Waybar restarted"
